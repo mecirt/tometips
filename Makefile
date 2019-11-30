d1c29f63a18b32681341b9c4a7aaeb6d1f0e057e
Tomas Mecir <mecirt@gmail.com>
2019-11-30 23:06:29 +0100

    new versions

--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@ LUA := luajit
 
 TOME_GIT_URL := http://git.net-core.org/tome/t-engine4.git
 
-RELEASE_VERSIONS := 1.4.9 1.5.9 1.6.0 1.6.1
+RELEASE_VERSIONS := 1.4.9 1.5.9 1.6.0 1.6.1 1.6.2 1.6.3 1.6.4
 VERSIONS := $(RELEASE_VERSIONS)
 
 # GitHub Pages output
--- versions.lua
+++ versions.lua
@@ -1,10 +1,12 @@
 
-latest_version = '1.6.1'
+latest_version = '1.6.4'
 
 versions = {}
 versions['1.4.9'] = { 'ashes-urhrok-1.0.5', 'orcs-1.0.3' }
 versions['1.5.9'] = { 'ashes-urhrok-1.0.6', 'orcs-1.0.4', 'possessors-1.5', 'cults-1.0.0' }
 versions['1.6.0'] = { 'ashes-urhrok-1.0.7', 'orcs-1.0.5', 'possessors-1.6', 'cults-1.0.3' }
 versions['1.6.1'] = { 'ashes-urhrok-1.0.8', 'orcs-1.1.0', 'possessors-1.6', 'cults-1.0.4' }
--- versions['master'] = { 'ashes-urhrok-1.0.6', 'orcs-1.0.4', 'possessors-1.5' }
+versions['1.6.2'] = { 'ashes-urhrok-1.0.8', 'orcs-1.1.2', 'possessors-1.6', 'cults-1.0.4' }
+versions['1.6.3'] = { 'ashes-urhrok-1.0.8', 'orcs-1.1.2', 'possessors-1.6', 'cults-1.0.4' }
+versions['1.6.4'] = { 'ashes-urhrok-1.0.8', 'orcs-1.1.2', 'possessors-1.6', 'cults-1.0.4' }
 
